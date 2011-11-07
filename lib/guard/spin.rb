5ab8c25455f9c9900a313bda9848a93428f768c1
Andrew Assarattanakul <assarata@gmail.com>
2011-11-07 09:55:47 -0600

    Changed the way the guard-spin works and will start and stop the spin server.

--- lib/guard/spin.rb
+++ lib/guard/spin.rb
@@ -5,10 +5,30 @@ module Guard
   class Spin < Guard
 
     autoload :Runner, 'guard/spin/runner'
+    attr_accessor :runner
+
+    def initialize(watchers=[], options={})
+      super
+      @runner = Runner.new(options)
+    end
+
+    def start
+      runner.kill_spin
+      runner.launch_spin("start")
+    end
+
+    def reload
+      runner.kill_spin
+      runner.launch_spin("reload")
+    end
 
     def run_on_change(paths)
-      Runner.new(paths).run
+      runner.run(paths)
     end
+
+    def stop
+      runner.kill_spin
+    end
+
   end
 end
-
--- lib/guard/spin/runner.rb
+++ lib/guard/spin/runner.rb
@@ -1,19 +1,46 @@
 module Guard
   class Spin
     class Runner
-      attr_reader :paths
+      attr_reader :options
 
-      def initialize(paths)
-        @paths = paths
+      def initialize(options = {})
+        @options = options
+        UI.info "Guard::Spin Initialized"
       end
 
-      def run
-        puts "Running #{paths.join(" ")}\n"
+      def launch_spin(action)
+        UI.info "#{action.caplitalize}ing Spin", :reset => true
+        start_spin
+      end
+
+      def kill_spin
+        UI.info "Killing spin", :reset => true
+        stop_spin
+      end
+
+      def run(paths)
+        UI.info "Running #{paths.join(" ")}", :reset => true
         system(run_command)
       end
 
+      private
+
       def run_command
         "spin push #{paths.join(" ")}"
+      end
+
+      def start_spin
+        system "spin serve"
+      end
+
+      def stop_spin
+        socket = UNIXSocket.open(socket_file)
+        socket.close
+      end
+
+      def socket_file
+        key = Digest::MD5.hexdigest([Dir.pwd, 'spin-gem'].join)
+        [Dir.tmpdir, key].join('/')
       end
     end
   end
