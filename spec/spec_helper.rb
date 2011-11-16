81d550660921a53bc7676fa8f64d39769b97c845
Andrew Assarattanakul <assarata@gmail.com>
2011-11-16 08:38:11 -0600

    Added basic spec helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require 'guard/spin'
+ENV['GUARD_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
